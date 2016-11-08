d93529c70b3b78f3ac3ec184d565b1728bbf0c5a
PepelyaevVladimir <pepelyaev.vm@gmail.com>
2016-11-08 16:23:09 +0500

    template+index

--- start/url.py
+++ start/url.py
@@ -1,6 +1,10 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
-def index(request):
-    return render(request, 'index.html')
+# Create your views here.
+from django.conf.urls import url
 
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+]
--- start/views.py
+++ start/views.py
@@ -1,10 +1,7 @@
+
+
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
-from django.conf.urls import url
-
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.index, name='index'),
-]
+def index(request):
+    return render(request, 'index.html')
