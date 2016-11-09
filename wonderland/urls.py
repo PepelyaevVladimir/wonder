72557e136689d0bdcf36e02ca7420dfbce0c1dd4
PepelyaevVladimir <pepelyaev.vm@gmail.com>
2016-11-09 12:50:04 +0500

    index5

--- wonderland/urls.py
+++ wonderland/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^', include('start.urls')),
+    url(r'^', include('start.url')),
     url(r'^admin/', admin.site.urls),
 ]
 
